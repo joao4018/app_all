d770d1e17d1491ef53f30cb2d0bc80cf2dc8a890
joao4018 <joao4018@gmail.com>
2022-01-03 15:37:18 -0300

    Added template

--- lib/main.dart
+++ lib/main.dart
@@ -1,35 +1,102 @@
-import 'package:all_app/screens/home.dart';
-import 'package:all_app/screens/signup.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:hexcolor/hexcolor.dart';
+import 'package:all_app/layout/cubit/cubit.dart';
+import 'package:all_app/layout/shop_layout.dart';
+import 'package:all_app/modules/login/shop_login_screen.dart';
+import 'package:all_app/modules/on_boarding/boarding_screen.dart';
+import 'package:all_app/shared/bloc_observer.dart';
+import 'package:all_app/shared/components/constants.dart';
+import 'package:all_app/shared/cubit/cubit.dart';
+import 'package:all_app/shared/cubit/states.dart';
+import 'package:all_app/shared/network/local/cache_helper.dart';
+import 'package:all_app/shared/network/remote/dio_helper.dart';
+import 'package:all_app/shared/styles/themes.dart';
+import 'package:splash_screen_view/SplashScreenView.dart';
 
-void main() {
-  runApp(const MyApp());
+void main() async {
+  // بيتأكد ان كل حاجه هنا في الميثود خلصت و بعدين يتفح الابلكيشن
+  WidgetsFlutterBinding.ensureInitialized();
+  Bloc.observer = MyBlocObserver();
+  DioHelper.init();
+  await CacheHelper.init();
+  bool? isDark = CacheHelper.getData(key: 'isDark');
+  Widget? widget;
+  bool ?onBoarding = CacheHelper.getData(key: 'onBoarding');
+   token = CacheHelper.getData(key: 'token');
+  if (onBoarding != null) {
+    if (token != null)
+      widget = ShopLayout();
+    else
+      widget = ShopLoginScreen();
+  } else {
+    widget = OnBoardingScreen();
+  }
+  runApp(MyApp(
+    isDark: isDark,
+    startWidget: widget,
+  ));
 }
+
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  final bool ?isDark;
+  final Widget? startWidget;
 
-  // This widget is the root of your application.
+  MyApp({
+    this.isDark,
+    this.startWidget,
+  });
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: SignUpPage(),
-      debugShowCheckedModeBanner: false,
-
-    );
+    return MultiBlocProvider(
+        providers: [
+          BlocProvider(
+            create: (context) => ShopCubit()
+              ..getHomeData()
+              ..getCategories()
+              ..getFavorites()
+              ..getUserData(),
+          ),
+          BlocProvider(
+            create: (BuildContext context) => AppCubit()
+              ..changeAppMode(
+                fromShared: isDark,
+              ),
+          ),
+        ],
+        child: BlocConsumer<AppCubit, AppStates>(
+          listener: (context, state) {},
+          builder: (context, state) {
+            return MaterialApp(
+              debugShowCheckedModeBanner: false,
+              theme: lightTheme,
+              darkTheme: darkTheme,
+              themeMode: AppCubit.get(context).isDark!
+                  ? ThemeMode.dark
+                  : ThemeMode.light,
+              home: SplashScreenView(
+                navigateRoute: startWidget,
+                duration: 5000,
+                imageSize: 130,
+                imageSrc: "assets/images/icon.png",
+                text: "Shop App",
+                textType: TextType.ColorizeAnimationText,
+                textStyle: const TextStyle(
+                  fontSize: 40.0,
+                ),
+                colors: const [
+                  Colors.purple,
+                  Colors.cyan,
+                  Colors.deepPurple,
+                  Colors.lightBlue,
+                ],
+                backgroundColor: AppCubit.get(context).isDark!
+                    ? HexColor('333739')
+                    : Colors.white,
+              ),
+            );
+          },
+        ));
   }
 }
-
